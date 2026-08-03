043a9a464496bfa02f5745a9089fe719c4168d52
chiksharma221-ui <chiksharma221@gmail.com>
2026-08-03 17:29:33 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (24.68%) - LeetSync

---
+++ 1737-maximum-nesting-depth-of-the-parentheses/maximum-nesting-depth-of-the-parentheses.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxDepth(string s) {
+        int depth=0,maxdepth=0;
+
+        for(char ch : s)
+        {
+            if(ch=='(')
+            {
+                depth++;
+                maxdepth=max(maxdepth,depth);
+            }
+            if(ch==')')
+            depth--;
+        }
+        return maxdepth;
+    }
+};
