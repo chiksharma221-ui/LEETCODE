33c4253f51e004ea4ced487f78fa13e833817dab
chiksharma221-ui <chiksharma221@gmail.com>
2026-08-07 16:20:20 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (81.67%) - LeetSync

---
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+        int n=s.size();
+        int count=0;
+        string ans="";
+        for(int i=0;i<n;i++)
+        {
+            if(s[i]==')')count-=1;
+            if(count!=0) ans+=s[i];
+            if(s[i]=='(') count+=1;
+        }
+        return ans;
+    }
+};
