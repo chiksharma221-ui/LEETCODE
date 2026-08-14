63b445baa69bca6c3f1ec928f407b95cd16f6971
chiksharma221-ui <chiksharma221@gmail.com>
2026-08-14 15:46:26 +0530

    Time: 0 ms (100.00%) | Memory: 16 MB (92.43%) - LeetSync

---
+++ 154-find-minimum-in-rotated-sorted-array-ii/find-minimum-in-rotated-sorted-array-ii.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        int n=nums.size();
+        int start=0,end=n-1;
+        int ans=INT_MAX;
+        while(start<end)
+        {
+            int mid=start+(end-start)/2;
+            if(nums[start]==nums[mid] && nums[mid]==nums[end])
+            {
+                ans=min(ans,nums[start]);
+                start++,end--;
+                continue;
+            }
+            if(nums[start]<=nums[mid])
+            {
+                ans=min(ans,nums[start]);
+                start=mid+1;
+            }
+            else{
+                ans=min(ans,nums[mid]);
+                end=mid-1;
+            }
+        }
+        ans=min(ans,nums[start]);
+        return ans;
+    }
+};
