f30669fff8f24972e4d8648ea8487a5fdaa596ee
chiksharma221-ui <chiksharma221@gmail.com>
2026-09-13 16:38:14 +0530

    Time: 0 ms (100.00%) | Memory: 34.5 MB (32.31%) - LeetSync

--- 3522-find-the-power-of-k-size-subarrays-i/find-the-power-of-k-size-subarrays-i.cpp
+++ 3522-find-the-power-of-k-size-subarrays-i/find-the-power-of-k-size-subarrays-i.cpp
@@ -2,10 +2,9 @@ class Solution {
 public:
     vector<int> resultsArray(vector<int>& nums, int k) {
         int n=nums.size();
-        int index=0;
         int first=0,second=0;
         vector<int>ans;
-        int maxi=-1e9;
+        int i=0;
         while(second<n)
         {
             while(second-first+1>k)
@@ -17,14 +16,19 @@ public:
                 bool valid=true;
                 for(int i=first;i<second;i++)
                 {
-                    if(nums[i+1]!=nums[i]+1)
+                    if(nums[i+1]-nums[i]!=1)
                     {
                         valid=false;
                         break;
                     }
                 }
-                if(valid) ans.push_back(nums[second]);
-                else ans.push_back(-1);
+                if(valid)
+                {
+                    ans.push_back(nums[second]);
+                }
+                else{
+                    ans.push_back(-1);
+                }
             }
             second++;
         }
