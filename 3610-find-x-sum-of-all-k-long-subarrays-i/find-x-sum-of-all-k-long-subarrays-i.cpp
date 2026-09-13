1fe55d1bd263b1f65b6908aaaec41ddd2378055d
chiksharma221-ui <chiksharma221@gmail.com>
2026-09-13 17:09:11 +0530

    Time: 3 ms (98.63%) | Memory: 32.7 MB (85.99%) - LeetSync

--- 3610-find-x-sum-of-all-k-long-subarrays-i/find-x-sum-of-all-k-long-subarrays-i.cpp
+++ 3610-find-x-sum-of-all-k-long-subarrays-i/find-x-sum-of-all-k-long-subarrays-i.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
-    int f(unordered_map<int,int>&mp,int x)
+    int f(unordered_map<int,int> &mp,int x)
     {
         priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
-        for(auto it : mp)
+
+        for(auto it:mp)
         {
             pq.push({it.second,it.first});
-
             if(pq.size()>x)
-            pq.pop();
+                pq.pop();
         }
         int sum=0;
         while(!pq.empty())
@@ -22,22 +22,19 @@ public:
     }
     vector<int> findXSum(vector<int>& nums, int k, int x) {
         unordered_map<int,int>mp;
-        int n=nums.size();
         int i=0,j=0;
+        int n=nums.size();
         vector<int>result;
         while(j<n)
         {
             mp[nums[j]]++;
-
             if(j-i+1==k)
             {
                 result.push_back(f(mp,x));
-
                 mp[nums[i]]--;
+
                 if(mp[nums[i]]==0)
-                {
                     mp.erase(nums[i]);
-                }
                 i++;
             }
             j++;
