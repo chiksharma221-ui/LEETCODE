89ae08868281445b97f1de486d696358f63dd1d7
chiksharma221-ui <chiksharma221@gmail.com>
2026-09-15 12:10:41 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (79.34%) - LeetSync

---
+++ 91-decode-ways/decode-ways.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int solve(int i,int &n,string &s,vector<int>&dp)
+    {
+        if(i==n)
+            return 1;
+        
+        if(s[i]=='0')
+            return 0;
+        if(dp[i]!=-1)
+            return dp[i];
+        
+        int result=solve(i+1,n,s,dp);
+        if(i+1<n)
+        {
+            if(s[i]=='1' || s[i]=='2' && s[i+1]<='6')
+            {
+                result+=solve(i+2,n,s,dp);
+            }
+        }
+        return dp[i]=result;
+    }
+    int numDecodings(string s) {
+        int n=s.size();
+        vector<int>dp(n,-1);
+        return solve(0,n,s,dp);
+    }
+};
